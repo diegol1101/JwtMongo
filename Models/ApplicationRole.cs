a9419ebc517a482a113ab76e474c880819f6e1e3
diegol1101 <ddiegoleal@gmail.com>
2023-10-26 20:26:16 -0500

    feat: :sparkles: se creo program

---
+++ Models/ApplicationRole.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace JwtMongo.Models
+{
+    [CollectionName("roles")]
+    public class ApplicationRole : MongoIdentityRole<Guid>
+    {
+        
+    }
+}
